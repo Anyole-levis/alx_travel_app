2c54e20ad0999393ba01fd71b71882ac9e6b6623
Anyole-levis <levisloprimo@gmail.com>
2025-06-23 13:25:17 +0300

    Update settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -1,1 +1,41 @@
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'listings',
+]
 
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    ...
+]
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
+DEBUG=True
+SECRET_KEY=your-secret-key
+DB_NAME=your_db_name
+DB_USER=your_db_user
+DB_PASSWORD=your_db_password
+DB_HOST=localhost
+DB_PORT=3306
+
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', default=False)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
